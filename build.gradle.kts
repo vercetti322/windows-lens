e45a38690d48b0fae9dfcbb32caf7d3d95910784
vercetti322 <vercettitommy322@gmail.com>
2026-08-09 17:12:59 +0530

    refactored global and module build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("org.springframework.boot") version "4.1.0" apply false
-    id("io.spring.dependency-management") version "1.1.7" apply false
-}
-
 group = "io.jatinjindal"
 version = "1.0.0-SNAPSHOT"
 
